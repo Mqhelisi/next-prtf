773ed0c27747472611dbdcdd4d2c5d3ae2ba015b
Mqhelisi <30624357+Mqhelisi@users.noreply.github.com>
2025-01-29 00:52:19 +0200

    SeptUpScene.tsx

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -25,7 +25,7 @@ function Loader() {
   const obj_sngl = useLoader(OBJLoader, `./${drwing}`, loader => {
     materials.preload()
   }) 
-    obj_sngl.setMaterials(materials)
+    // obj_sngl.setMaterials(materials)
     
 //   obj_sngl.materialLibraries(`./${mtrl}`)
 
